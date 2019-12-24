4806caa862d1fe290f4f569ccb6feb2e5c2afec4
Yui Yukihira <yuiyukihira1@gmail.com>
2019-12-24 12:51:05 +0000

    Add dynamic fading to posts

--- src/Post.tsx
+++ src/Post.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as _ from 'lodash';
 
 import {Placement, SubIcon} from './SubIcon';
 import Image from 'react-bootstrap/Image';
@@ -23,26 +24,39 @@ interface PostProps {
     fade: boolean
 }
 
+export class Post extends React.Component<PostProps, string> {
+    constructor(props: PostProps) {
+        super(props);
+        this.state = _.uniqueId('Post');
+    }
 
+    componentDidMount() {
+        const height = document.getElementById(this.state)?.clientHeight;
+        console.log(height);
+        if (height != null && height >= 255) {
+            document.getElementById(this.state)?.classList.add("fade-away")
+        }
+    }
 
-export function Post(props: PostProps) {
-    return <div className="post">
+    render() {
+        return <div className="post">
         <div className="post-meta">
             <SubIcon
-            name={props.sub.name}
-            icon={props.sub.icon}
+            name={this.props.sub.name}
+            icon={this.props.sub.icon}
             positions={[Placement.Right, Placement.Bottom, Placement.Top, Placement.Left]}
             />
         </div>
-        <div className={`post-body ${props.fade ? "fade-away" : ""}`}>{props.content}</div>
+        <div id={this.state} className="post-body">{this.props.content}</div>
         <div className="post-options">
             <div>
                 <div><img src="/angle-up-solid.svg" alt="upvote" /></div>
                 <div><img src="/angle-down-solid.svg" alt="downvote" width="10px"/></div>
             </div>
-            <div>{readablizeNumber(props.upvotes)}</div>|
-            <div>{readablizeNumber(props.comments)}</div>|
+            <div>{readablizeNumber(this.props.upvotes)}</div>|
+            <div>{readablizeNumber(this.props.comments)}</div>|
             <div>Share</div>
         </div>
     </div>;
+    }
 }
